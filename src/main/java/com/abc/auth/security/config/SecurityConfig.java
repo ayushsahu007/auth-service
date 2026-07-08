fe0fa6047514ae5721ea6600ce85d329ebed015d
ayushsahu007 <ayushsahu007ay@gmail.com>
2026-07-08 13:04:11 +0530

    Sprint 3.2:Spring Security Configuration Complete

--- src/main/java/com/abc/auth/security/config/SecurityConfig.java
+++ src/main/java/com/abc/auth/security/config/SecurityConfig.java
@@ -1,12 +1,17 @@
 package com.abc.auth.security.config;
 
-
 import com.abc.auth.security.jwt.JwtAuthenticationFilter;
+import com.abc.auth.security.user.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -19,27 +24,30 @@ public class SecurityConfig {
     private final JwtAccessDeniedHandler accessDeniedHandler;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http)
-            throws Exception {
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http,
+            DaoAuthenticationProvider authenticationProvider
+    ) throws Exception {
 
         http
                 .csrf(csrf -> csrf.disable())
 
                 .sessionManagement(session ->
-                        session.sessionCreationPolicy(
-                                SessionCreationPolicy.STATELESS))
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
 
                 .exceptionHandling(exception -> exception
                         .authenticationEntryPoint(authenticationEntryPoint)
                         .accessDeniedHandler(accessDeniedHandler)
                 )
-                .authorizeHttpRequests(auth -> auth
 
+                .authenticationProvider(authenticationProvider)
+
+                .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/api/v1/auth/register",
                                 "/api/v1/auth/login"
                         ).permitAll()
-
                         .anyRequest().authenticated()
                 )
 
@@ -51,4 +59,29 @@ public class SecurityConfig {
         return http.build();
     }
 
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(
+            CustomUserDetailsService userDetailsService,
+            PasswordEncoder passwordEncoder
+    ) {
+
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder);
+
+        return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration configuration
+    ) throws Exception {
+
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
--- src/main/java/com/abc/auth/service/impl/AuthServiceImpl.java
+++ src/main/java/com/abc/auth/service/impl/AuthServiceImpl.java
@@ -1,18 +1,21 @@
 package com.abc.auth.service.impl;
 
 import com.abc.auth.Repository.UserRepository;
-import com.abc.auth.dto.request.RegisterRequest;
-import com.abc.auth.dto.response.RegisterResponse;
 import com.abc.auth.dto.request.LoginRequest;
+import com.abc.auth.dto.request.RegisterRequest;
 import com.abc.auth.dto.response.LoginResponse;
+import com.abc.auth.dto.response.RegisterResponse;
 import com.abc.auth.exception.DuplicateResourceException;
-import com.abc.auth.exception.InvalidCredentialsException;
-import com.abc.auth.security.constants.SecurityConstants;
-import com.abc.auth.security.jwt.JwtService;
 import com.abc.auth.model.User;
 import com.abc.auth.security.config.JwtProperties;
+import com.abc.auth.security.constants.SecurityConstants;
+import com.abc.auth.security.jwt.JwtService;
+import com.abc.auth.security.user.CustomUserDetails;
 import com.abc.auth.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -25,18 +28,18 @@ public class AuthServiceImpl implements AuthService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
     private final JwtProperties jwtProperties;
-
 
     @Override
     public RegisterResponse register(RegisterRequest request) {
 
-        String email = request.getEmail().trim().toLowerCase(); // for email address save lowercase
+        String email = request.getEmail().trim().toLowerCase();
 
         Map<String, String> errors = new HashMap<>();
 
-        if (userRepository.existsByEmail(request.getEmail())) {
+        if (userRepository.existsByEmail(email)) {
             errors.put("email", "Email already registered.");
         }
 
@@ -48,35 +51,37 @@ public class AuthServiceImpl implements AuthService {
             throw new DuplicateResourceException(errors);
         }
 
-        // Create User Entity
         User user = User.builder()
                 .firstName(request.getFirstName())
                 .lastName(request.getLastName())
                 .email(email)
                 .phone(request.getPhone())
                 .password(passwordEncoder.encode(request.getPassword()))
-//                .status(UserStatus.ACTIVE)
-//                .emailVerified(false)  we use default @PrePersist
                 .build();
 
-        // Save User
         userRepository.save(user);
 
-        // Response
-        return new RegisterResponse(
-                "Registration successful."
-        );
+        return new RegisterResponse("Registration successful.");
     }
 
     @Override
     public LoginResponse login(LoginRequest request) {
+
         String email = request.getEmail().trim().toLowerCase();
 
-          User user =   userRepository.findByEmail(email).orElseThrow(()-> new InvalidCredentialsException("Invalid email or password"));
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(
+                        email,
+                        request.getPassword()
+                );
 
-          if (!passwordEncoder.matches(request.getPassword(),user.getPassword())){
-              throw new InvalidCredentialsException("Invalid email or password");
-          }
+        Authentication authentication =
+                authenticationManager.authenticate(authenticationToken);
+
+        CustomUserDetails userDetails =
+                (CustomUserDetails) authentication.getPrincipal();
+
+        User user = userDetails.getUser();
 
         String accessToken = jwtService.generateAccessToken(
                 user.getId(),
@@ -93,6 +98,4 @@ public class AuthServiceImpl implements AuthService {
                 .expiresIn(jwtProperties.getAccessTokenExpiration() / 1000)
                 .build();
     }
-
-
 }
